7acbdc17fef4fd25b10f3992954c3b1f048dcbf4
zonyitoo <zonyitoo@gmail.com>
2013-05-18 14:20:47 +0800

    fix error

--- mkpkg.sh
+++ mkpkg.sh
@@ -21,7 +21,7 @@ cp auth/qh3client-daemon-script pkg/$INIT_PATH/qh3client-daemon
 cp auth/com.qh3client.EAPDaemon.conf pkg/$DBUS_SYSTEMD
 cp auth/com.qh3client.EAPDaemon.service pkg/$DBUS_SERVICE
 cp gui/qh3client pkg/$BIN_PATH
-cp gui/qH3Client.desktop pkg/$SYSTEM_PIXMAP
+cp gui/icon.png pkg/$SYSTEM_PIXMAP/qH3Client.png
 cp gui/qH3Client.desktop pkg/$APP_PATH
 
 cp -r debian pkg/DEBIAN
